cd9d181cbe77882e0c6bd20527ada50c5c469858
chiumanfelix <chiumanfelix@gmail.com>
2022-03-23 21:46:21 +0700

    ver 1.4.3 (Toast FragmentKedua.kt added)

--- app/src/main/java/com/example/binarchallengech3/FragmentKedua.kt
+++ app/src/main/java/com/example/binarchallengech3/FragmentKedua.kt
@@ -5,12 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.example.binarchallengech3.databinding.FragmentKeduaBinding
 
 class FragmentKedua : Fragment() {
-    private var _binding: FragmentKeduaBinding? = null
-    private val binding get() = _binding!!
+    var _binding: FragmentKeduaBinding? = null
+    val binding get() = _binding!!
 
     companion object{
         val EXTRA_NAME = "EXTRA_NAME"
@@ -29,10 +30,14 @@ class FragmentKedua : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnToFragment3.setOnClickListener {
+            if (binding.etName.text.isNotEmpty()){
             val mBundle = Bundle()
             val name = binding.etName.text.toString()
             mBundle.putString(EXTRA_NAME, name)
             it.findNavController().navigate(R.id.action_fragmentKedua_to_fragmentKetiga, mBundle)
+            }else{
+                Toast.makeText(requireContext(), "Ada Kolom Yang Kosong", Toast.LENGTH_SHORT).show()
+            }
         }
     }
     override fun onDestroy() {
